32d6fd843ce62ff779b492bb48d627dc985e2f5c
tusharkalamkar7 <tusharkalamkar7@gmail.com>
2018-01-17 21:52:06 +0530

    Update DepartmentDAO.java

--- DepartmentDAO.java
+++ DepartmentDAO.java
@@ -13,6 +13,50 @@ public class DepartmentDAO implements DepartmentDAOI{
    	 
    	 return dept;
 	}
+	
+	private static void createTable() throws SQLException {
+
+		Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+
+		String createTableSQL = "CREATE TABLE Department ("
+				+"deptId NUMBER NOT NULL AUTO_INCREMENT,"
+				+"deptName varchar(30),"
+				+"createdDateTime date,"
+				+"updatedDateTime date,"
+				+"createdId NUMBER,"
+				+"updatedId NUMBER,"
+				+"PRIMARY KEY (deptId)"
+				+")";
+
+		try {
+			dbConnection = ConnectionFactory.getConnection();
+			preparedStatement = dbConnection.prepareStatement(createTableSQL);
+
+			System.out.println(createTableSQL);
+
+			// execute create SQL stetement
+			preparedStatement.executeUpdate();
+
+			System.out.println("Table Department is created!");
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+
+	}
 
 	public User getDepartment(int id) {
     Connection connection = connectionFactory.getConnection();
