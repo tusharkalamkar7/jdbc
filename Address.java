70d600a07c89e2ecc34416afd2fb0a97c87c2b27
tusharkalamkar7 <tusharkalamkar7@gmail.com>
2018-01-17 21:39:33 +0530

    Update Address.java

--- Address.java
+++ Address.java
@@ -27,28 +27,65 @@ public class Address {
 	this.updatedId=updatedId;
     }
   
-    public Integer getAge() {
-        return age;
-    }
-    public void setAge(Integer age) {
-        this.age = age;
-    }
     public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPass() {
-        return pass;
-    }
-    public void setPass(String pass) {
-        this.pass = pass;
-    }
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLine1() {
+		return line1;
+	}
+	public void setLine1(String line1) {
+		this.line1 = line1;
+	}
+	public String getLine2() {
+		return line2;
+	}
+	public void setLine2(String line2) {
+		this.line2 = line2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Integer getPincode() {
+		return pincode;
+	}
+	public void setPincode(Integer pincode) {
+		this.pincode = pincode;
+	}
+	public Date getCreatedDateTime() {
+		return createdDateTime;
+	}
+	public void setCreatedDateTime(Date createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+	public Date getUpdatedDateTime() {
+		return updatedDateTime;
+	}
+	public void setUpdatedDateTime(Date updatedDateTime) {
+		this.updatedDateTime = updatedDateTime;
+	}
+	public Integer getCreatedId() {
+		return createdId;
+	}
+	public void setCreatedId(Integer createdId) {
+		this.createdId = createdId;
+	}
+	public Integer getUpdatedId() {
+		return updatedId;
+	}
+	public void setUpdatedId(Integer updatedId) {
+		this.updatedId = updatedId;
+	}
+
 }
