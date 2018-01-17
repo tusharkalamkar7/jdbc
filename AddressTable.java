d84eac27260456d49a6545801385ba8fd7ec1135
tusharkalamkar7 <tusharkalamkar7@gmail.com>
2018-01-17 19:29:31 +0530

    Update AddressTable.java

--- AddressTable.java
+++ AddressTable.java
@@ -7,11 +7,6 @@ import java.sql.SQLException;
 
 public class JDBCPreparedStatementCreateExample {
 
-	private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:orcl";
-	private static final String DB_USER = "user";
-	private static final String DB_PASSWORD = "password";
-
 	public static void main(String[] argv) {
 
 		try {
@@ -47,7 +42,7 @@ public class JDBCPreparedStatementCreateExample {
 				+")";
 
 		try {
-			dbConnection = getDBConnection();
+			dbConnection = ConnectionFactory.getConnection();
 			preparedStatement = dbConnection.prepareStatement(createTableSQL);
 
 			System.out.println(createTableSQL);
@@ -72,36 +67,6 @@ public class JDBCPreparedStatementCreateExample {
 			}
 
 		}
-
-	}
-
-	private static Connection getDBConnection() {
-
-		Connection dbConnection = null;
-
-		try {
-
-			Class.forName(DB_DRIVER);
-
-		} catch (ClassNotFoundException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		try {
-
-			dbConnection = DriverManager.getConnection(
-                            DB_CONNECTION, DB_USER,DB_PASSWORD);
-			return dbConnection;
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return dbConnection;
 
 	}
 
