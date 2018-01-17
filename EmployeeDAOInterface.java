a4b9a71cc9d515b72fe4af804e657ffa98c2bc67
tusharkalamkar7 <tusharkalamkar7@gmail.com>
2018-01-17 11:41:03 +0530

    Delete EmployeeDAOInterface.java

--- EmployeeDAOInterface.java
+++
@@ -1,7 +0,0 @@
-package com.project.dao;
-public interface EmployeeDAOInterface{
-        public void save(int empNo, String name, double sal, int dNo);
-        public boolean updateSal(int empNo, double newSal);
-        public String getEmp(int empNo);
-        boolean delete(int empno);
-}
