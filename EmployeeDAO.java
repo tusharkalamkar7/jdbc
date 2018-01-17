b4dc45340d764deff006d33d0787956c9fc9d59b
tusharkalamkar7 <tusharkalamkar7@gmail.com>
2018-01-17 21:53:43 +0530

    Update EmployeeDAO.java

--- EmployeeDAO.java
+++ EmployeeDAO.java
@@ -15,8 +15,56 @@ public class EmployeeDAO implements EmployeeDAOI{
    	 emp.setUpdatedId(rs.getInt("updatedId"));
    	 return emp;
 	}
+	
+	private static void createTable() throws SQLException {
 
-	public User getEmployee(int id) {
+		Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+
+		String createTableSQL = "CREATE TABLE Employee ("
+					+"empId NUMBER NOT NULL AUTO_INCREMENT,"
+					+"firstName varchar(30),"
+					+"lastName varchar(30),"
+					+"mobNo NUMBER,"
+					+"deptId NUMBER,"
+					+"createdDateTime date,"
+					+"updatedDateTime date,"
+					+"createdId NUMBER,"
+					+"updatedId NUMBER,"
+					+"PRIMARY KEY (empId),"
+					+"FOREIGN KEY (deptId) REFERENCES Department(deptId)"
+					+")";
+
+		try {
+			dbConnection = ConnectionFactory.getConnection();
+			preparedStatement = dbConnection.prepareStatement(createTableSQL);
+
+			System.out.println(createTableSQL);
+
+			// execute create SQL stetement
+			preparedStatement.executeUpdate();
+
+			System.out.println("Table Employee is created!");
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+
+	}
+
+public User getEmployee(int id) {
     Connection connection = connectionFactory.getConnection();
     try {
         Statement stmt = connection.createStatement();
